7d426e668cf3bb800a927c7cd1727d1aaaa0ae1d
josen <JosenChina@126.com>
2018-03-09 16:48:16 +0800

    2018/03/09

--- JosenBlog.py
+++ JosenBlog.py
@@ -2,8 +2,15 @@
 # filename: JosenBlog.py
 import os
 from webapp import create_app
+from flask import redirect, url_for
 
 app = create_app(os.environ.get('FLASKY_CONFIG') or 'default')
+
+
+@app.route('/')
+def Index():
+    return redirect(url_for('main.index'))
+
 
 if __name__ == '__main__':
     app.run()
